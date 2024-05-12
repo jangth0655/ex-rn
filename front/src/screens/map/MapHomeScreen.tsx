247be0aed18170930972545c8fb29b05a589f8eb
TAEHEE <jangth0655@gmail.com>
2024-05-12 12:08:28 +0900

    permission setting

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,12 +1,22 @@
-import {Button, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import useAuth from '@/hooks/queries/useAuth';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default function MapHomeScreen() {
   const {logoutMutation} = useAuth();
   return (
-    <View>
-      <Text>맵 스크린</Text>
-      <Button title="로그아웃" onPress={() => logoutMutation.mutate()} />
-    </View>
+    <MapView
+      style={styles.container}
+      provider={PROVIDER_GOOGLE}
+      showsUserLocation
+      followsUserLocation
+      showsMyLocationButton={false}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
